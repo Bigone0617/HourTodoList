2c2ef473654bccce0128c47a031cc974b25e51b3
Bigone0617 <xodlfsha@gmail.com>
2021-03-24 18:02:20 +0900

    add done&modify btn  && add clock

--- title.js
+++ title.js
@@ -1,13 +1,26 @@
 const titleContainer = document.getElementById("titleContainer");
 
-let Today = new Date().toDateString();
-let Year = Today.slice(11,15);
-let Month = Today.slice(4,7);
-let Day = Today.slice(8,10);
+let Today = new Date();
+let Year = Today.toDateString().slice(11,15);
+let Month = Today.toDateString().slice(4,7);
+let Day = Today.toDateString().slice(8,10);
 
 const timeElement = document.createElement("h1");
 const timeText = document.createTextNode(Year + " - " + Month + " - " + Day);
 
 timeElement.appendChild(timeText);
 
+let clock = document.createElement("h3");
+let clockText = document.createTextNode(Today.getHours() + "시 " + Today.getMinutes() + "분 " + Today.getSeconds());
+clock.appendChild(clockText);
+
+
 titleContainer.appendChild(timeElement);
+titleContainer.appendChild(clock);
+
+setInterval(() => {
+    let time = new Date();
+    clockText = document.createTextNode(time.getHours() + "시 " + time.getMinutes() + "분 " + time.getSeconds());
+    clock.textContent = "";
+    clock.appendChild(clockText);
+}, 1000);
--- todoList.js
+++ todoList.js
@@ -1,15 +1,31 @@
 const todoListContainer = document.getElementById("todoListContainer");
 
-const saveTodo = (time) => {
+const saveTodo = (time, isDone) => {
     const value = document.getElementById("input"+time).value;
-
-    debugger;
 
     if(localStorage.getItem(time+"todo") !== null){
         localStorage.removeItem(time+"todo");
     }
 
-    localStorage.setItem(time+"todo", value);
+    localStorage.setItem(time+"todo", JSON.stringify({value : value, isDone : isDone}));
+}
+
+const setValue = (time) => {
+    const values = localStorage.getItem(time+"todo");
+
+    if(values !== null ){
+        const inputBox = document.getElementById("input"+time)
+        inputBox.value = JSON.parse(values).value;
+
+        if(JSON.parse(values).isDone){
+            inputBox.readOnly = true;
+            inputBox.style.background = "#A9A9A9";
+
+            document.getElementById("save"+time).hidden = true;
+            document.getElementById("modify"+time).hidden = false;
+        }
+    }
+    
 }
 
 const makeTodo = (time) => {
@@ -28,28 +44,78 @@ const makeTodo = (time) => {
     timeNode.style.marginRight ="10px";
     timeNode.style.float = "left";
 
-    
+    timeNode.appendChild(timeText);
+
+    // input
     let todoInput = document.createElement("input");
 
     todoInput.id = "input" + time;
     
-
+    // 저장 버튼
     let saveBtn = document.createElement("button");
-    let btnText = document.createTextNode("Save");
-    saveBtn.appendChild(btnText);
+    let btnSaveText = document.createTextNode("Save");
+    saveBtn.appendChild(btnSaveText);
+
+    saveBtn.id = "save"+time;
 
     saveBtn.addEventListener("click", function(){
-        saveTodo(time);
+        saveTodo(time, false);
     });
 
-    timeNode.appendChild(timeText);
+    // 완료 버튼
+    let doneBtn = document.createElement("button");
+    let btnDoneText = document.createTextNode("Done");
+    doneBtn.appendChild(btnDoneText);
+
+    doneBtn.id = "done"+time;
+
+    // 수정 버튼
+    let modifyBtn = document.createElement("button");
+    let btnModifyText = document.createTextNode("Modify");
+    modifyBtn.appendChild(btnModifyText);
+
+    modifyBtn.id = "modify"+time;
+    modifyBtn.hidden = true;
+
+    // 완료버트 클릭
+    doneBtn.addEventListener("click", function(){
+        // save 버튼 숨기기
+        saveBtn.hidden = true;
+        
+        // 인풋 막기
+        todoInput.readOnly = true;
+        todoInput.style.backgroundColor = "#A9A9A9";
+
+        // 수정버튼 보이기
+        modifyBtn.hidden = false;
+
+        saveTodo(time, true);
+    });
+
+    modifyBtn.addEventListener("click", function(){
+        // save 버튼 보이기
+        saveBtn.hidden = false;
+        
+        // 인풋 입력가능하게 풀기
+        todoInput.readOnly = false;
+        todoInput.style.backgroundColor = null;
+
+        // 수정버튼 숨기기
+        modifyBtn.hidden = true;
+    });
+
     
     todoNode.appendChild(timeNode);
     todoNode.appendChild(todoInput);
     todoNode.appendChild(saveBtn);
+    todoNode.appendChild(doneBtn);
+    todoNode.appendChild(modifyBtn);
 
     // 부모 컨테이너에 붙이기
-    document.getElementById("todoListContainer").appendChild(todoNode);
+    todoListContainer.appendChild(todoNode);
+
+    // 저장된 todo가 있으면 값 넣어주기
+    setValue(time);
 }
 
 const makeList = () => {
